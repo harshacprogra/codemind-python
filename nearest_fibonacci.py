c7d88dbbe556d0d2ba19807de40ba738bdcd2b5d
harshacprogra <99875579+harshacprogra@users.noreply.github.com>
2023-07-07 11:22:10 +0530

    n=int(input()) fib=[0,1] while fib[-1] < n:     fib.append(fib[-1]+fib[-2]) if n in fib:     print(n) elif fib[-1]-n<n-fib[-2]:     print(fib[-1]) elif fib[-1]-n>n-fib[-2]:     print(fib[-2]) else:     print(fib[-2],fib[-1])

--- nearest_fibonacci.py
+++ nearest_fibonacci.py
@@ -1,6 +1,16 @@
 n=int(input())
 fib=[0,1]
-while fib[-1]<n:
+while fib[-1] < n:
     fib.append(fib[-1]+fib[-2])
 if n in fib:
     print(n)
+elif fib[-1]-n<n-fib[-2]:
+    print(fib[-1])
+elif fib[-1]-n>n-fib[-2]:
+    print(fib[-2])
+else:
+    print(fib[-2],fib[-1])
+
+    
+    
+        
